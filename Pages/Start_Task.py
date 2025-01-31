d18842b7174c9d6677c3c2047374f10ae0f36a5a
aakash-test7 <akgdsc7@gmail.com>
2025-01-31 21:45:23 +0530

    Update Start_Task.py

--- Pages/Start_Task.py
+++ Pages/Start_Task.py
@@ -7,16 +7,18 @@ def start_task_page():
     col1, col2 = st.columns(2)
 
     with col1:
-        tid = st.text_input("Enter the Gene ID: ", placeholder="e.g., Ca_00001", key="Tid_input1").strip()
-        mtid = st.text_input("Enter multiple Gene IDs: ", placeholder="e.g., Ca_00001, Ca_00002", key="mTid_input2").strip()
+        con1=st.container(border=True)
+        tid = con1.text_input("Enter the Gene ID: ", placeholder="e.g., Ca_00001", key="Tid_input1").strip()
+        mtid = con1.text_input("Enter multiple Gene IDs: ", placeholder="e.g., Ca_00001, Ca_00002", key="mTid_input2").strip()
         if mtid:
             mtid_list = [item.strip() for item in mtid.replace(",", " ").split()]
             mtid_list = list(set(mtid_list))
             mtid = ",".join(mtid_list)
 
     with col2:
-        locid = st.text_input("Enter the NCBI ID: ", placeholder="e.g., LOC101511858", key="Locid_input1").strip()
-        mlocid = st.text_input("Enter multiple NCBI IDs: ", placeholder="e.g., LOC101511858, LOC101496413", key="mLocid_input2").strip()
+        con2=st.container(border=True)
+        locid = con2.text_input("Enter the NCBI ID: ", placeholder="e.g., LOC101511858", key="Locid_input1").strip()
+        mlocid = con2.text_input("Enter multiple NCBI IDs: ", placeholder="e.g., LOC101511858, LOC101496413", key="mLocid_input2").strip()
         if mlocid:
             mlocid_list = [item.strip() for item in mlocid.replace(",", " ").split()]
             mlocid_list = list(set(mlocid_list))
